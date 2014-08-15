c783db4a6445363240a1042fd2a43ea2f281549f
lmeilibr <leandro.meili@gmail.com>
2014-08-15 13:17:03 -0300

    ProgramminAssigment2 - Solution
    first solution to assigment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix: 
+## cacheSole: This function
 
-## Write a short comment describing this function
+## This function creates an object, with setters and getters for a Matrix and
+## its inverse.
+## The input argument is a 2x2 matrix
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(mtx = matrix()) {
+        cachedInverse <- NULL
+        set <- function(newMtx){
+                mtx <<- newMtx
+                cachedInverse <<- NULL
+        }
+        get <- function() mtx
+        
+        ## never use this directly, only internal calculation
+        setInverse <- function(solve) cachedInverse <<- solve 
+        
+        getInverse <- function() cachedInverse
+        list(set = set, get = get,
+             setInverse = setInverse,
+             getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## This function checks if the inverse Matrix of the argument was calculate. If
+## TRUE, returns the cached value, if FALSE, it calculates and return the 
+## inverse matrix of the argument. The input is a object from the 
+## makeCacheMatrix object.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(mtx) {
+        cached <- mtx$getInverse()
+        if(!is.null(cached)) {
+                message("getting cached data")
+                return(cached)
+        }
+        data <- mtx$get()
+        cachedInverse <- solve(data)
+        mtx$setInverse(cachedInverse)
+        cachedInverse
 }
